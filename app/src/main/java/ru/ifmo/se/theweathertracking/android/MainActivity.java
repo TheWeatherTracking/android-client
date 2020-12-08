22e9e9ba38b546aeb18126df7bb7a0f84081fb56
Ekaterina <kartasheva.ekaterina.elets@gmail.com>
2020-12-08 22:33:18 +0300

    Hello World!

---
+++ app/src/main/java/ru/ifmo/se/theweathertracking/android/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.ifmo.se.theweathertracking.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
