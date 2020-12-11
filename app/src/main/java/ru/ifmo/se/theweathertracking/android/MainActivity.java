d6f1a047827cb9b9793358ea196587767f3b746c
lizlop <242433@niuitmo.ru>
2020-12-12 01:32:05 +0300

    fixes for successful build

--- app/src/main/java/ru/ifmo/se/theweathertracking/android/MainActivity.java
+++ app/src/main/java/ru/ifmo/se/theweathertracking/android/MainActivity.java
@@ -1,29 +1,42 @@
 package ru.ifmo.se.theweathertracking.android;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import ru.ifmo.se.theweathertracking.util.PropertiesManager;
 
+public class MainActivity extends BaseActivity {
+
+    private final String tag = "Main activity";
+    private PropertiesManager propertiesManager;
     private AppBarConfiguration mAppBarConfiguration;
+
+    @Override
+    protected String getTag() {
+        return this.tag;
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected int getActivityTitle() {
+        return R.string.app_name;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -38,6 +51,12 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        propertiesManager = new PropertiesManager(getApplicationContext());
+        if (!propertiesManager.hasValidToken()) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 
     @Override
